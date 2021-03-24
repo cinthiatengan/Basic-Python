4495a50a2865d5a2856f1188327592fed3e65064
Cinthia <cinthia.tengan@gmail.com>
2021-03-24 17:23:39 -0300

    finished exercise about types

--- Ex_Files_Python_EssT/Exercise Files/Chap02/class.py
+++ Ex_Files_Python_EssT/Exercise Files/Chap02/class.py
@@ -2,15 +2,18 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 class Duck:
+    sound = 'Quaaaack!'
+    walking = 'Walks like a duck.'
+    
     def quack(self):
-        print('Quaaack!')
+        print(self.sound)
 
     def walk(self):
-        print('Walks like a duck.')
+        print('self.walking')
 
 def main():
-    donald = Duck()
-    donald.quack()
-    donald.walk()
+    donald = Duck()   # donald eh um objeto da classe duck
+    donald.quack()    # chama o metodo quack
+    donald.walk()     # chama o meoto walk
 
 if __name__ == '__main__': main()
--- Ex_Files_Python_EssT/Exercise Files/Chap03/types.py
+++ Ex_Files_Python_EssT/Exercise Files/Chap03/types.py
@@ -1,6 +1,48 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-x = 7
+from decimal import *
+
+x = 'seven'.upper()
 print('x is {}'.format(x))
 print(type(x))
+
+y = 'seven'.capitalize()
+print('y is {}'.format(y))
+print(type(y))
+
+z = 'seven {1:<9} {0:>9}'.format(8,9)
+print('z is {}'.format(z))
+print(type(z))
+
+w = 'seven {} {}'.format(8,9)
+print('w is {}'.format(w))
+print(type(w))
+
+v = 'seven {1} {0}'.format(8,9)  #muda a posição
+print('v is {}'.format(v))
+print(type(v))
+
+u = 'seven {1:<09} {0:>09}'.format(8,9)  #adiciona espaços, no caso zeros
+print('u is {}'.format(u))
+print(type(u))
+
+a=9
+b=8
+i = f'seven {a} {b}'            #olha esse caraio de f kkkkkkkkkkkkkkkkkkkkk se nao coloca ele nao chama fstrings se chama
+print('i is {}'.format(i))
+print(type(i))
+
+j = f'seven {a:<09} {b:>09}'            
+print('j is {}'.format(j))
+print(type(j))
+
+k = 7*4.35678 #coisas de python3
+print('k is {}'.format(k))
+print(type(k))
+
+n=Decimal('.10')
+m=Decimal('.30')
+f = n+n+n-m
+print('f is {}'.format(f))
+print(type(f))
