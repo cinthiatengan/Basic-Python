9e20d360e842186114818eba6c1b9a298964fbcf
Cinthia <cinthia.tengan@gmail.com>
2021-03-24 15:26:04 -0300

    conditionals class

--- Ex_Files_Python_EssT/Exercise Files/Chap02/blocks.py
+++ Ex_Files_Python_EssT/Exercise Files/Chap02/blocks.py
@@ -4,7 +4,14 @@
 x = 42
 y = 73
 
-if x > y:
+if x < y:
+    z = 112 #a variavel possui escopo mesmo fora do bloco que ela se encontra !!!!
     print('x < y: x is {} and y is {}'.format(x, y))
-    
-print('something else')
+    print('adeus vida de madao')
+    print('agora é trabalhar 9h por dia, 5 dia por semana')
+    print('gosto de: trabalhar')
+    print('não gosto de: trabalhar')
+elif x == y:
+    print('x == y: x is {} and y is {}'.format(x, y))
+else:    
+    print('x is {}'.format(x))
