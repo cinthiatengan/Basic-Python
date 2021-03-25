80a54364fdb60b859f3b0d53b9691329d1ad52b1
Cinthia <cinthia.tengan@gmail.com>
2021-03-25 14:04:32 -0300

    chapter 8 dict

--- Ex_Files_Python_EssT/Exercise Files/Chap08/dict.py
+++ Ex_Files_Python_EssT/Exercise Files/Chap08/dict.py
@@ -2,9 +2,15 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 def main():
+    friends = dict(Joao = 'aoba', Correa = 'ai credo', Nita = "au au")
     animals = { 'kitten': 'meow', 'puppy': 'ruff!', 'lion': 'grrr',
         'giraffe': 'I am a giraffe!', 'dragon': 'rawr' }
+    #for v in friends.values(): print(v)
+    #animals['lion'] = 'I am a lion'
+    #animals["bird"] = 'piu piu'
+    print('found!' if 'lion' in animals else 'nope!')
     print_dict(animals)
+    
 
 def print_dict(o):
     for x in o: print(f'{x}: {o[x]}')
