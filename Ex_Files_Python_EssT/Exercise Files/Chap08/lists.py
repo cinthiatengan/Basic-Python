f5bd69e90959a9857b162532a8309104f9e21339
Cinthia <cinthia.tengan@gmail.com>
2021-03-25 13:52:30 -0300

    chapter 8 lists

--- Ex_Files_Python_EssT/Exercise Files/Chap08/lists.py
+++ Ex_Files_Python_EssT/Exercise Files/Chap08/lists.py
@@ -2,7 +2,13 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 def main():
-    game = [ 'Rock', 'Paper', 'Scissors', 'Lizard', 'Spock' ]
+    game = [ 'Rock', 'Paper', 'Scissors', 'Lizard', 'Spock' ] #List
+    print(game[1:3]) #pode se usar o metodo de 3 parametros
+    i= game.index('Paper')
+    #game.append("João")
+    #game.insert(0, "Corea")
+    print(', '.join(game)) # coloca uma virgula entre os elementos da lista
+    print(len(game))
     print_list(game)
 
 def print_list(o):
