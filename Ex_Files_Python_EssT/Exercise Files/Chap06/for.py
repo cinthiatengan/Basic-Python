d87b5131b66456ef884cea25f36d3ff34ab4e112
Cinthia <cinthia.tengan@gmail.com>
2021-03-25 10:52:52 -0300

    end of chapter 6 loops

--- Ex_Files_Python_EssT/Exercise Files/Chap06/for.py
+++ Ex_Files_Python_EssT/Exercise Files/Chap06/for.py
@@ -4,5 +4,13 @@
 animals = ( 'bear', 'bunny', 'dog', 'cat', 'velociraptor' )
 
 for pet in animals:
+    if pet == 'dog':continue
+    #if pet == 'velociraptor': break
     print(pet)
+else:
+    print('that is all the animals')    
 
+#generation = ('Bruno', 'Chris', 'Correa', 'Leo', 'joão', 'Thi')
+
+#for friend in generation:
+    #print(friend)
--- Ex_Files_Python_EssT/Exercise Files/Chap06/while.py
+++ Ex_Files_Python_EssT/Exercise Files/Chap06/while.py
@@ -3,6 +3,15 @@
 
 secret = 'swordfish'
 pw = ''
+auth = False
+count = 0
+max_attempt = 5
 
 while pw != secret:
-    pw = input("What's the secret word? ")
+    count += 1
+    if count > max_attempt: break
+    pw = input(f"{count}: What's the secret word? ")
+else:
+    auth = True
+
+print("Authorized" if auth else "CHAMA OS POLIÇA")
