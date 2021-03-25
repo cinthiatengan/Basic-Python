a8e95873c68103cc0b8a4eb2f78d391871af6bd5
Cinthia <cinthia.tengan@gmail.com>
2021-03-25 09:53:51 -0300

    begining of chapter 5 operators

--- Ex_Files_Python_EssT/Exercise Files/Chap04/ternary.py
+++ Ex_Files_Python_EssT/Exercise Files/Chap04/ternary.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-hungry = True
+hungry = False
 x = 'Feed the bear now!' if hungry else 'Do not feed the bear.'
 print(x)
+
+fome = True
+y= "Alimente o Bruno!" if fome else "não alimente o Bruno!"
+print(y)
--- Ex_Files_Python_EssT/Exercise Files/Chap05/arithmetic.py
+++ Ex_Files_Python_EssT/Exercise Files/Chap05/arithmetic.py
@@ -5,4 +5,15 @@ x = 5
 y = 3
 z = x + y
 
+a = y - x
+b = y/x
+c = x*y
+d = y//x
+e = x + y
+
 print(f'result is {z}')
+print(f'result is {a}')
+print(f'result is {b}')
+print(f'result is {c}')
+print(f'result is {d}')
+print(f'result is {e}')
