07937e1a59d71313675e61167a6647c5ec2c3238
Cinthia <cinthia.tengan@gmail.com>
2021-03-26 17:17:02 -0300

    chapter 11 string object begining

--- Ex_Files_Python_EssT/Exercise Files/Chap11/hello.py
+++ Ex_Files_Python_EssT/Exercise Files/Chap11/hello.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-print('Hello, World.')
+print('Hello, World.'.upper())
+print('Hello, World.'.swapcase())
+print('Hello, World. {}'.format(3*2))
+
+print("""
+      HELL IS
+      HERE.
+      
+      {}
+      
+      """.format(2*1015))
+
+s='FORA BOZO. {}'
+print(s.format(2*8.5))
+
+class MyString(str):
+    def __str__(self):
+        return self[::-1]
+
+s= MyString('GENOCIDA BASCULHO')
+print(s)
