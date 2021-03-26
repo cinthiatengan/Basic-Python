705bbc990a90ebcf882b31bc76ca0a51f4532166
Cinthia <cinthia.tengan@gmail.com>
2021-03-26 16:13:08 -0300

    chapter 10 exceptions try catch

--- Ex_Files_Python_EssT/Exercise Files/Chap10/hello.py
+++ Ex_Files_Python_EssT/Exercise Files/Chap10/hello.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
+import sys
+
 def main():
-    print('Hello, World.')
+    try:
+        x = 5/0
+    except ValueError:
+        print("I caught a ValueError!")
+    except:
+        print(f"unknown error: {sys.exc_info()[1]}")
+    else:
+        print("good job!")
+        print("This is the valor of {}".format(x))
+
 
 if __name__ == '__main__': main()
