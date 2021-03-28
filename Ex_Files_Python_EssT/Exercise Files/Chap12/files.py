f10be1942889bcf23ba64fd2063c68ebf1774e86
Cinthia <cinthia.tengan@gmail.com>
2021-03-28 11:15:16 -0300

    chapter 12 end

--- Ex_Files_Python_EssT/Exercise Files/Chap12/files.py
+++ Ex_Files_Python_EssT/Exercise Files/Chap12/files.py
@@ -2,7 +2,7 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 def main():
-    f = open('lines.txt')
+    f = open('lines.txt') # segundo argumento r= read, w = write, a = append r+ = read and write, r+t = binary mode
     for line in f:
         print(line.rstrip())
 
