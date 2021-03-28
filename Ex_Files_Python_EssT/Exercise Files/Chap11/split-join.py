b9a410cc380a543c11dea23a755dadc174c3a281
Cinthia <cinthia.tengan@gmail.com>
2021-03-28 10:55:34 -0300

    chapter 11 string object end split and join

--- Ex_Files_Python_EssT/Exercise Files/Chap11/split-join.py
+++ Ex_Files_Python_EssT/Exercise Files/Chap11/split-join.py
@@ -2,4 +2,10 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 s = 'This is a long string with a bunch of words in it.'
-print(s)
+l = s.split()
+s2 = ":".join(l)
+print(s.split())
+print(s.split('i'))
+print(s2)
+print(l)
+
