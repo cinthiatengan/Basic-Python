5d4dd002b2b715106f567830f02abbd27cb8828b
Cinthia <cinthia.tengan@gmail.com>
2021-03-28 11:19:06 -0300

    chapter 13 begin

--- Ex_Files_Python_EssT/Exercise Files/Chap12/copy-bin.py
+++ Ex_Files_Python_EssT/Exercise Files/Chap12/copy-bin.py
@@ -5,7 +5,7 @@ def main():
     infile = open('berlin.jpg', 'rb')
     outfile = open('berlin-copy.jpg', 'wb')
     while True:
-        buf = infile.read(10240)
+        buf = infile.read(10240) #size of the buffer
         if buf:
             outfile.write(buf)
             print('.', end='', flush=True)
--- Ex_Files_Python_EssT/Exercise Files/Chap13/types.py
+++ Ex_Files_Python_EssT/Exercise Files/Chap13/types.py
@@ -2,7 +2,12 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 x = '47'
-y = int(x)
+y = int(x) # constructor q transforma a string em int conversao
+z = float(x)
+a = abs(x)
+b = divmod(x, 3)
+c = complex()
+d = x+34j
 
 print(f'x is {type(x)}')
 print(f'x is {x}')
