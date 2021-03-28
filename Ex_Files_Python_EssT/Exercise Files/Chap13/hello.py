8753a89cba570e7cfc8fad147471597be88285eb
Cinthia <cinthia.tengan@gmail.com>
2021-03-28 11:31:54 -0300

    chapter 13 begin

--- Ex_Files_Python_EssT/Exercise Files/Chap13/hello.py
+++ Ex_Files_Python_EssT/Exercise Files/Chap13/hello.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-print('Hello, World.')
+#class bunny:
+    #def __init__(self, n):
+        #self.n = n
+    #def __repr__(self):
+        #return f'repr: the number of bunnies is {self._n}'
+    #def __str__(self):
+        #return f'str: the number of bunnies is {self._n}'
+    
+#s = bunny(47)
+#print(repr(s)) # string representation of a value
+
+x = (1, 2, 3, 4, 5)
+y = len(x) # mostra o tamanho de x
+z = list(reversed(x))
+a = sum(x) # max e min tem o any q retorna true or false
+print(x)
+print(y)
+print(z)
+print(a)
