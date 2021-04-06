04c7e1c7844b9f7a1eeb9c908ed7e5709fe5c8cd
Cinthia <cinthia.tengan@gmail.com>
2021-04-06 11:22:10 -0300

    exercise training fixed

--- Ex_Files_Python_EssT/Exercise Files/Chap01/rockPaperScissors.py
+++ Ex_Files_Python_EssT/Exercise Files/Chap01/rockPaperScissors.py
@@ -7,28 +7,28 @@ import os
 import re
 os.system('cls' if os.name=='nt' else clear)
 while (1< 2):
-    print "\n"
-    print "Rock, Paper, Scissors - Shoot!"
+    print ("\n")
+    print ("Rock, Paper, Scissors - Shoot!")
     userChoice = raw_input("Choose your weapon [R]ock, [P]aper, or [S]cissors: ")
     if not re.match("[SsRrPp]", userChoice):
-        print "Please choose a letter: "
-        print "[R]ock, [P]aper, or [S]cissors."
+        print ("Please choose a letter: ")
+        print ("[R]ock, [P]aper, or [S]cissors.")
         continue
     # Echo the user's choice
-    print "You choose: " + userChoice
+    print ("You choose: " + userChoice)
     choices = ['R', 'P', 'S']
     opponentChoice = random.choice(choices)
-    print "I choose: " + opponentChoice
+    print ("I choose: " + opponentChoice)
     if opponentChoice == str.upper(userChoice):
-        print "Tie!"
+        print ("Tie!")
     elif opponentChoice == 'R' and userChoice.upper() == 'S':
-        print "Rock beats Scissors, I lose"
+        print ("Rock beats Scissors, I lose")
         continue
-    elif opponentChoice == 'S' and userChoice.upper() == 'P'
-        print " Scissors beats paper, I lose"
+    elif opponentChoice == 'S' and userChoice.upper() == 'P':
+        print ("Scissors beats paper, I lose")
         continue
-    elif opponentChoice == 'P' and userChoice.upper() == 'R'
-        print "Paper beats rock, I lose"
+    elif opponentChoice == 'P' and userChoice.upper() == 'R':
+        print ("Paper beats rock, I lose")
         continue
     else:
-        print "I win!"
+        print ("I win!")
