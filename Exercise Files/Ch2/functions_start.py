e8a75798833289325669b9f59d8e54b007128ca2
Cinthia <cinthia.tengan@gmail.com>
2021-04-27 13:52:35 -0300

    added modifications to the exercise 2

--- Exercise Files/Ch2/functions_start.py
+++ Exercise Files/Ch2/functions_start.py
@@ -3,25 +3,38 @@
 #
 
 # define a basic function
-def func1():
-    print("Eu sou uma função")
+# def func1():
+#     print("Eu sou uma função")
 
 # function that takes arguments
-def func2(arg1, arg2):
-    print(arg1, " ", arg2)
+# def func2(arg1, arg2):
+#     print(arg1, " ", arg2)
 
 # function that returns a value
-def cube(x):
-    return x*x*x
+# def cube(x):
+#     return x*x*x
 
 # function with default value for an argument
-
+# def power(num, x=1):
+#     result = 1
+#     for i in range(x):
+#         result = result*num
+#     return result
 
 #function with variable number of arguments
+def multi_add(*args):     # o asteristico (*) simboliza que muitos parametros podem ser adicionados
+    result=0
+    for x in args:
+        result = result+x
+    return result
 
 # func1()          #a função é chamada diretamente
 # print(func1())   #
 # print(func1)
-func2(10,20)
-print(func2(10,20))
-print(cube(3))
+# func2(10,20)
+# print(func2(10,20))
+# print(cube(3))
+# print(power(2))
+# print(power(2,3))
+# print(power(x=3, num=2))
+print(multi_add(10,4,5,10,4))
