57415853e34c1e3ab83cd1dfbbec0a3f9b711e68
Cinthia <cinthia.tengan@gmail.com>
2021-04-27 14:48:00 -0300

    added modifications to the exercise 4 classes

--- Exercise Files/Ch2/classes_start.py
+++ Exercise Files/Ch2/classes_start.py
@@ -1,10 +1,29 @@
 #
 # Example file for working with classes
 #
+class myClass():
+    def method1(self):
+        print("MyClass method1")
 
+    def method2(self, someString):
+        print("myClass method2" + someString)
+
+class anotherClass():
+    def method1(self):
+        myClass.method1(self)
+        print("anotherClass method1")
+
+    def method2(self, someString):
+        print("anotherClass method2 " + someString)
 
 def main():
+    c = myClass()
+    c.method1()
+    c.method2("This is a string")
 
+    c2 = anotherClass()
+    c2.method1()
+    c2.method2("Isso é uma string ")
 
 if __name__ == "__main__":
     main()
