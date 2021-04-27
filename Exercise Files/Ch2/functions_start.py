9db4610379172bcb7417cb647e6987b056c4f23b
Cinthia <cinthia.tengan@gmail.com>
2021-04-27 13:33:02 -0300

    added modifications to the exercise 1

--- Exercise Files/Ch2/functions_start.py
+++ Exercise Files/Ch2/functions_start.py
@@ -3,7 +3,8 @@
 #
 
 # define a basic function
-
+def func1():
+    print("Eu sou uma função")
 
 # function that takes arguments
 
@@ -16,4 +17,6 @@
 
 #function with variable number of arguments
 
-
+func1()          #a função é chamada diretamente
+print(func1())   #
+print(func1)
