66a366966f03dcb3b524bf41fead6a36ab2807b4
Cinthia <cinthia.tengan@gmail.com>
2021-04-27 18:00:13 -0300

    added modifications to the exercise 5 date

--- Exercise Files/Ch3/dates_start.py
+++ Exercise Files/Ch3/dates_start.py
@@ -1,24 +1,32 @@
 #
 # Example file for working with date information
 #
-
+from datetime import date
+from datetime import time
+from datetime import datetime
 
 def main():
   ## DATE OBJECTS
   # Get today's date from the simple today() method from the date class
-
+  # today = date.today()
+  # print("A data de hoje é ", today)
 
   # print out the date's individual components
-
+  # print("Componentes da data: ", today.day, today.month, today.year)
   
   # retrieve today's weekday (0=Monday, 6=Sunday)
-
+  # print("O dia da semana hoje é: ", today.weekday())
+  # days=["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
+  # print("que é dia da semana: ", days[today.weekday()])
   
   ## DATETIME OBJECTS
   # Get today's date from the datetime class
-
+  today = datetime.now()
+  print("a data de hoje é: ", today)
   
   # Get the current time
+  t= datetime.time(datetime.now())
+  print("A hora exata de hoje é: ", t)
 
  
 
