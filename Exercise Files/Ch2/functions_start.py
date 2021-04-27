fdca4d955af0177770d7d411ed89794badfab8f4
Cinthia <cinthia.tengan@gmail.com>
2021-04-27 13:36:13 -0300

    added modifications to the exercise 1.1

--- Exercise Files/Ch2/functions_start.py
+++ Exercise Files/Ch2/functions_start.py
@@ -7,16 +7,21 @@ def func1():
     print("Eu sou uma função")
 
 # function that takes arguments
-
+def func2(arg1, arg2):
+    print(arg1, " ", arg2)
 
 # function that returns a value
-
+def cube(x):
+    return x*x*x
 
 # function with default value for an argument
 
 
 #function with variable number of arguments
 
-func1()          #a função é chamada diretamente
-print(func1())   #
-print(func1)
+# func1()          #a função é chamada diretamente
+# print(func1())   #
+# print(func1)
+func2(10,20)
+print(func2(10,20))
+print(cube(3))
