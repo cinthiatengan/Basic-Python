c7ec441b92cd61203557df0d4d55402919d7ba83
Cinthia <cinthia.tengan@gmail.com>
2021-04-27 11:31:06 -0300

    added config and first exercise

--- Exercise Files/Ch2/helloworld_start.py
+++ Exercise Files/Ch2/helloworld_start.py
@@ -1,3 +1,12 @@
 #
 # Example file for HelloWorld
 #
+
+def main():
+    print("Hello babaca")
+    name = input("Qual teu nome? ")
+    print("Nice to meet you, ", name)
+
+
+if __name__ == "__main__":
+    main()
