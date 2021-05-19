5a19b4cb261c7b092defda06c6529590388f82f2
Cinthia <cinthia.tengan@gmail.com>
2021-05-19 16:42:33 -0300

    chapter 5 html parsing

--- Exercise Files/Ch5/htmlparsing_start.py
+++ Exercise Files/Ch5/htmlparsing_start.py
@@ -1,11 +1,16 @@
 # 
 # Example file for parsing and processing HTML
 #
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
 
 
 def main():
   # instantiate the parser and feed it some HTML
   parser = MyHTMLParser()
+  f = open("samplehtml.html")
+  if 
     
 
 if __name__ == "__main__":
