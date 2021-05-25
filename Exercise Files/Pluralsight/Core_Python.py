d6f3f388dccf176c1297bd044c3998cc8f2396e2
Cinthia <cinthia.tengan@gmail.com>
2021-05-25 08:57:47 -0300

    new course pluralsight

--- Exercise Files/Pluralsight/Core_Python.py
+++ Exercise Files/Pluralsight/Core_Python.py
@@ -1,3 +1,12 @@
 # help(str)
 
 # Lists
+# a = []
+# a.append(1)
+# a.append(2)
+# a.append('Pituco')
+# print(a)
+
+# pets = ["Pituco", "Tico", "Gisele", "Totoro", "Bolt", "Spike"]
+# for pet in pets:
+#     print(pet)
